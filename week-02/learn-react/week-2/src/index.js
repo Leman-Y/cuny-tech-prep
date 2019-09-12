166ab9c729b8bd558fd2f0cdb6e7160fad189e9f
Solepride <leman.yan80@myhunter.cuny.edu>
2019-09-12 14:19:44 -0400

    Finished up to step 11 for React tutorial

--- week-02/learn-react/week-2/src/index.js
+++ week-02/learn-react/week-2/src/index.js
@@ -1,28 +1,74 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 
 
+/*
+class App extends React.Component {
+  constructor(props) {
+    super(props); // required so that props work
+    this.state = {
+      clicks: 0,
+    };
+  }
 
-function Student(props) {
-  return <div>- { props.name } is in class</div>;
+  handleClick(event) {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>The button has been clicked { this.state.clicks } times</p>
+        <button onClick={ (e) => this.handleClick(e) }>{ this.props.btnText }!!!</button>
+      </div>
+    );
+  }
 }
-
-function Welcome() {
-  return (
-    <div>
-      <h1>Welcome CTP Class</h1>
-      <p>Who is in class?</p>
-      <Student name="Sally" />
-      <Student name="Bob" />
-      <Student name="Fergie"/>
-    </div>
-  );
-}
-
 
 ReactDOM.render(
-  <Welcome />,
+  <App btnText="Click me" />,
   document.getElementById("root")
 );
+*/
 
+class Board extends React.Component {
+  render() {
+    let className = "board";
+    if (this.props.selected) {
+      className += " selected";
+    }
+    return (
+      <div className={className}>
+        {this.props.index + 1}
+      </div>
+    );
+  }
+}
 
+class BoardSwitcher extends React.Component {
+  render() {
+    let boards = [];
+    for (let ii = 0; ii < this.props.numBoards; ii++) {
+      let isSelected = ii === 0;
+      boards.push(
+        <Board index={ii} selected={isSelected} key={ii} />
+      );
+    }
+    
+    return (
+      <div>
+        <div className="boards">{boards}</div>
+        <button>Toggle</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <BoardSwitcher numBoards={3} />,
+  document.getElementById('container')
+);
