ca2556f3d44fa625f557587db104ef96050bb0b4
Solepride <leman.yan80@myhunter.cuny.edu>
2019-09-19 21:05:10 -0400

    Editing zip search

--- week-03-projects/zip-search/src/App.js
+++ week-03-projects/zip-search/src/App.js
@@ -8,19 +8,47 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (
+  <div>
+    <input 
+      type="text"
+      placeholder="Try 10001"
+      onChange={props.onChange} 
+    />   
+  </div>);
 }
 
 
 class App extends Component {
+  onZipSearchFieldChange = (event) => {
+    const zipcode = event.target.value; 
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${zipcode}`)
+      .then(response => {
+        if(response.ok)
+        {
+          return response.json();
+        }
+      })
+      .then(cities => {
+        console.log(cities);
+      })
+      .catch(error => {
+        console.log(`Error`, error);
+      })
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-          <h3>Zip Code:</h3>
-        <ZipSearchField/>
+      {/*    <h3>Zip Code:</h3>   
+            <zipSearchField onChange={ (event) => {this.oonZipSearchFieldChange(event) } } />
+      */}
+        <ZipSearchField onChange={this.onZipSearchFieldChange} />
+
         <div>
           <City />
           <City />
