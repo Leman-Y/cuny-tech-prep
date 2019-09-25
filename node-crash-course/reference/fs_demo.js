ba312e90fa344f7fa8e017e269df6bb61134913f
Solepride <leman.yan80@myhunter.cuny.edu>
2019-09-25 19:15:22 -0400

    Done with path & fs examples

--- node-crash-course/reference/fs_demo.js
+++ node-crash-course/reference/fs_demo.js
@@ -10,6 +10,9 @@ fs.mkdir(path.join(__dirname, '/test/'), {} , err => {
 */
 
 //Create and write to file - overwrites
+// control + shift + L to select multiple lines and edit
+// Select vertically right click + shift and move cursor
+/*
 fs.writeFile(path.join(__dirname, '/test/', 'hello.txt'), 'Hello World!' , err => {
 	if(err) throw err;
 	console.log('File written to...');
@@ -18,10 +21,52 @@ fs.writeFile(path.join(__dirname, '/test/', 'hello.txt'), 'Hello World!' , err =
 	fs.appendFile(path.join(__dirname, '/test/', 'hello.txt'), ' I love Node.js' , err => {
 	if(err) throw err;
 	console.log('File written to...');
-});
+	});
 
 
 });
+*/
+
+/*
+//Read file
+fs.readFile(path.join(__dirname, '/test/', 'hello.txt'), 'utf8' , (err, data) => {
+	if(err) throw err;
+	console.log(data);
+});
+*/
+
+//Read file
+fs.rename(path.join(__dirname, '/test/', 'hello.txt'), path.join(__dirname, '/test/', 'helloworld.txt') , (err) => {
+	if(err) throw err;
+	console.log('File renamed...');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
