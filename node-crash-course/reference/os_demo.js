baead3518d4270c8fb4c20e474be2d38cc4cf951
Solepride <leman.yan80@myhunter.cuny.edu>
2019-09-25 19:46:02 -0400

    Learned url module

---
+++ node-crash-course/reference/os_demo.js
@@ -0,0 +1,24 @@
+const os = require('os');
+//os module, can create some cool apps with this stuff
+
+//Platform
+console.log(os.platform());
+
+//CPU Arch
+console.log(os.arch());
+
+//CPU Core Info
+console.log(os.cpus());
+
+//Free Memory
+console.log(os.freemem());
+
+//Total Memory
+console.log(os.totalmem());
+
+//Home dir
+console.log(os.homedir());
+
+//Uptime
+console.log(os.uptime());
+
