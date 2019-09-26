594fe8a4f7bdfc1a16a1b66222bd796e77bb1225
Solepride <leman.yan80@myhunter.cuny.edu>
2019-09-25 22:21:25 -0400

    Learning about events

---
+++ node-crash-course/logger.js
@@ -0,0 +1,6 @@
+const EventEmitter = require('events');
+const uuid = require('uuid') //Creates random universal
+/*
+(Universal Unique Identifier) is a 128-bit number used to uniquely identify some object or entity on the Internet. Depending on the specific mechanisms used, a UUID is either guaranteed to be different or is, at least, extremely likely to be different from any other UUID generated until 3400 A.D. A standard of generating ids, standardized by the Open Software Foundation (OSF).
+
+*/
---
+++ node-crash-course/reference/event_demo.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+
+//Create class
+class MyEmitter extends EventEmitter {
+
+}
+
+//Init object
+const myEmitter = new MyEmitter();
+
+//Event listener 
+myEmitter.on('event', () => console.log('Event Fired!'));
+
+//Init Event
+myEmitter.emit('event');
+
