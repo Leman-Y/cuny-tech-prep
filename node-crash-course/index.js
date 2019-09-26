020725a0bee93e4e8b4b546f145d1c803fa9da98
Solepride <leman.yan80@myhunter.cuny.edu>
2019-09-25 22:36:19 -0400

    Learning http module

--- node-crash-course/index.js
+++ node-crash-course/index.js
@@ -12,3 +12,11 @@ const person1 = new person(`Bob Lazar`, 99);
 person1.greeting();
 
 
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log(`Called Listener:`, data));
+
+logger.log('Hello World');
+
--- node-crash-course/logger.js
+++ node-crash-course/logger.js
@@ -4,3 +4,14 @@ const uuid = require('uuid') //Creates random universal
 (Universal Unique Identifier) is a 128-bit number used to uniquely identify some object or entity on the Internet. Depending on the specific mechanisms used, a UUID is either guaranteed to be different or is, at least, extremely likely to be different from any other UUID generated until 3400 A.D. A standard of generating ids, standardized by the Open Software Foundation (OSF).
 
 */
+
+//console.log(uuid.v4());
+
+class Logger extends EventEmitter {
+	log(msg){
+		//Call event
+		this.emit('message',{id: uuid.v4(), msg:msg});
+	}
+}
+
+module.exports = Logger; 
