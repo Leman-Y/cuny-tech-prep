7ad473fb249c626a87d46c58c56cbc7a116da2b7
Solepride <leman.yan80@myhunter.cuny.edu>
2019-10-19 15:36:40 -0400

    Done with express crash course

--- express-crash-course/index.js
+++ express-crash-course/index.js
@@ -2,13 +2,20 @@
 
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const logger = require('./middleware/logger');
+const exphbs = require('express-handlebars');
+const members = require('./Members');
 //Need to import modules like c++ libraries
 
 const app = express();
 
+//Passport.js authentication
 
+/*
+Handling requests
+Handling http statuses
+Handling responses, middleware, templates
+*/
 
 //Init middleware
 //app.use(logger);
@@ -20,20 +27,53 @@ app.get('/', (req,res) => {
 });
 */
 
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
+//Body Parser Middleware
+//Without this you get nothing back, need to use a body parser to parse the data in the body
+//Used to need 3rd party body parser but now you can use express's middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-//Gets All Members 
-app.get('/api/members', (req,res) => res.json(members));
+//Homepage Route
+app.get('/', (req, res) => res.render('index', {
+	title: 'Member App',
+	members
+}));
 
-//Get Single Member
-app.get('/api/members/:id', (req,res) => {
-	//res.send(req.params.id);
-	res.json(members.filter(member => member.id === parseInt(req.params.id)) );
-});
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
