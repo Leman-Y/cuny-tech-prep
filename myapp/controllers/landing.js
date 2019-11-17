7879609b697f814556912c2bb9a841bbca347c60
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-17 15:23:23 -0500

    WE NOW HAVE A DATABASE AND CAN DISPLAY IT!!! COOL!
    Implement /leads route
    Show a collection of leads upon submission

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -13,9 +13,14 @@ exports.submit_lead = function(req, res, next) {
 	({
 		email: req.body.lead_email
 	}).then(lead => {
-		res.redirect('/');
+		res.redirect('/leads');
 	})
 }
 
-
+//findall defined by sequelize
+exports.show_leads = function(req, res, next) {
+	return models.Lead.findAll().then(leads => {
+	  res.render('landing', { title: 'Express', leads: leads });
+	})
+}
 
