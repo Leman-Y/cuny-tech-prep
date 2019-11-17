fc53134916169908f114a3f83fb7eb65b6c87b9b
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-17 15:59:07 -0500

    Add a new route to display lead details

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -24,3 +24,16 @@ exports.show_leads = function(req, res, next) {
 	})
 }
 
+//params - parameters
+exports.show_lead = function(req, res, next) {
+	return models.Lead.findOne({
+		where : {
+			id : req.params.lead_id
+		}
+	}).then(lead => {
+		res.render('lead', { lead : lead });
+	})
+
+
+}
+
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -7,5 +7,7 @@ let landing = require('../controllers/landing')
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
 router.get('/leads', landing.show_leads);
+router.get('/lead/:lead_id', landing.show_lead); //unique path component 
+// I forgot the / in /lead and that messed things up. Is there a better way to know your errors? Or debug? 
 
 module.exports = router;
