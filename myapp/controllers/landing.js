b0e814737c13a38eeb142cd4fe53ab4d977f5c89
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-17 15:15:02 -0500

    Save leads collected on landing page
    We save email addresses collected

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -1,11 +1,20 @@
 
+const models = require('../models');
+//allow to get sequelize methods
+
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 }
 
 exports.submit_lead = function(req, res, next) {
 	console.log("lead email:", req.body.lead_email);
-	res.redirect('/');
+
+	return models.Lead.create
+	({
+		email: req.body.lead_email
+	}).then(lead => {
+		res.redirect('/');
+	})
 }
 
 
