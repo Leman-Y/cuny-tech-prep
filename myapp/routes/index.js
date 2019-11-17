e00ee0bf84dc12ef5af7b97b7dd62841703ae924
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-17 16:17:42 -0500

    Add lead edit and post routes

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -9,5 +9,8 @@ router.post('/', landing.submit_lead);
 router.get('/leads', landing.show_leads);
 router.get('/lead/:lead_id', landing.show_lead); //unique path component 
 // I forgot the / in /lead and that messed things up. Is there a better way to know your errors? Or debug? 
+router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', landing.edit_lead);  //submit form to update the lead id
+
 
 module.exports = router;
