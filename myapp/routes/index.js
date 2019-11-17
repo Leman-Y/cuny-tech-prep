baaa6c317a09833596d73156e88c6e47a1e73942
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-17 18:27:19 -0500

    Implement a lead delete route using ajax

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -12,6 +12,6 @@ router.get('/lead/:lead_id', landing.show_lead); //unique path component
 router.get('/lead/:lead_id/edit', landing.show_edit_lead);
 router.post('/lead/:lead_id/edit', landing.edit_lead);  //submit form to update the lead id
 router.post('/lead/:lead_id/delete', landing.delete_lead); 
-
+router.post('/lead/:lead_id/delete-json', landing.delete_lead_json);
 
 module.exports = router;
