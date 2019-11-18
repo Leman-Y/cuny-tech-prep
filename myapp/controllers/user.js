8b8902a758dd14b2f38eac7ad38f250b391ea3b6
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-17 21:04:59 -0500

    Install packages for login and sign up
    installed passportjs passport-local validator bcrypt
    express-session connect-f

--- myapp/controllers/user.js
+++ myapp/controllers/user.js
@@ -7,3 +7,16 @@ exports.show_login = function(req, res, next){
 exports.show_signup = function(req, res, next){
 	res.render('user/signup', { formData: {}, errors: {} });
 }
+ 
+/*
+Implmement signup and login route using node js library called passport js
+Implements authentiation, could use social networks or other means
+Can go to passportjs.org for more details
+
+npm install passport passport-local bcrypt validator express-session connect-flash --save
+*/ 
+exports.signup = function(req, res, next){
+
+}
+
+exports.login = funtion 
